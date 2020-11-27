69def98e90edbe57b3e7fa3123cf18fa6037c9bb
Shankar <shangan.23@gmail.com>
2020-11-27 22:55:48 +0530

    feat(auth): Plain form with angular form implemented

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  loginForm: FormGroup;
+  constructor(private builder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.loginForm = this.builder.group({
+      userName: ['', Validators.required],
+      password: ['', Validators.required]
+    });
   }
 
+  authenticate() {
+    console.log(this.loginForm.value);
+  }
 }
--- src/app/module/auth.module.ts
+++ src/app/module/auth.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from '../auth/login/login.component';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     LoginComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports:[
     LoginComponent

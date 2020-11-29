394e36eff05fdedb9694ce3b9972f1a37e93bf8b
Shankar <shangan.23@gmail.com>
2020-11-29 22:08:16 +0530

    feat(http-client): Added http module

--- src/app/module/auth.module.ts
+++ src/app/module/auth.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from '../auth/login/login.component';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { FormsModule,ReactiveFormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   exports:[
     LoginComponent

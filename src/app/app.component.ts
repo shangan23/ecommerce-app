e7aa0e4fe11c7a75230313a3905ff74cd32e61ec
Shankar <shangan.23@gmail.com>
2020-12-02 16:27:41 +0530

    feat(middleware): Added router middleware

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import {
+  Router,
+  Event,
+  NavigationStart,
+  NavigationError,
+  NavigationEnd
+} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +14,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Ecommerce App';
+
+  constructor(private router: Router) {
+    //Router subscriber
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationStart) {
+        //do something on start activity
+        console.info('NavigationStart', event);
+      }
+
+      if (event instanceof NavigationError) {
+        // Handle error
+        console.error(event.error);
+      }
+
+      if (event instanceof NavigationEnd) {
+        //do something on end activity
+        console.info('NavigationEnd', event);
+      }
+    });
+  }
 }
